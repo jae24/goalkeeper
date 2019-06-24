04b78d304cd93d4f4653292dd11ba6443d6dc1f2
jae24 <jae.park510@gmail.com>
2019-06-24 02:11:27 -0700

    Added a gitignore file

--- client/src/Components/Goals.js
+++ client/src/Components/Goals.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import Goal from './Goal';
 
-export default ({goals}) => (
-  <div className="row jumbotron">
-    {goals.map((goal, index)=>(
-      <Goal
-        key={index}
-        hoursPerDay={goal.hoursPerDay}
-        daysLeft={goal.daysLeft}
-        completion={goal.completion}
-        name={goal.name}
-        description={goal.description}/>
-      )
-    )}
-  </div>
-)
+class Goals extends React.Component {
+
+state = {
+  goals: this.props.goals
+}
+
+componentDidMount(){
+  console.log(this.state);
+}
+
+render (){
+  return (
+    <div className="row jumbotron">
+      {
+        this.state.goals.map((goal, index)=>(
+        <Goal
+          key={index}
+          hoursPerDay={goal.hoursPerDay}
+          daysLeft={goal.daysLeft}
+          completion={goal.completion}
+          name={goal.name}
+          description={goal.description}/>
+        ))
+      }
+    </div>
+    )
+  }
+}
+
+export default Goals;
