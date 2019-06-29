fe9dff64d5c403f7ef3a7f156335b2883ad346b9
jae24 <jae.park510@gmail.com>
2019-06-28 23:56:07 -0700

    Added scss pre-processor for stylesheets and updated layout on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Goals from './Components/Goals'
 import Header from './Components/Header'
-// import Footer from './Components/Footer'
+import Footer from './Components/Footer'
 import About from './Components/About'
 import Home from './Components/Home'
 import GoalPage from './Components/GoalPage'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'react-dates/lib/css/_datepicker.css';
+import './App.scss'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/about" exact component={About}/>
           <Route path="/goals/:id" exact component={GoalPage}/>
         </Switch>
+        <Footer/>
       </div>
     </Router>
   );
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,55 +1,83 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => (
-  <div>
+  <div className="home-wrapper">
     <div className="hero matte-panel index-panel">
       <div className="container">
         <div className="hero-text">
           <div>Set Your Goals. </div>
           <div>Achieve Them. </div>
+          <button className="create-account-btn" style={{fontSize: '20px'}}>
+            <Link to="/goals">Create an Account</Link>
+          </button>
         </div>
       </div>
     </div>
-    <div className="info1 fresh-panel index-panel">
-      <div className="container">
-        <div className="row info-box">
-          <div className="info-text col-md-4">
-            <div className="pic-div">
-              <img className="info-icon" src="https://image.flaticon.com/icons/png/512/236/236810.png"/>
-            </div>
-            <h3 className="info-box-title">Accountability</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
-          <div className="info-text col-md-4">
-            <div className="pic-div">
-              <img className="info-icon" src="https://image.flaticon.com/icons/svg/236/236804.svg"/>
-            </div>
-            <h3 className="info-box-title">Consistency</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
-          <div className="info-text col-md-4">
-            <div className="pic-div">
-              <img className="info-icon" src="https://image.flaticon.com/icons/svg/236/236804.svg"/>
-            </div>
-            <h3 className="info-box-title">Feedback</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
+    <div className="clear-panel index-panel">
+      <div className="row" style={{paddingTop : '150px'}}>
+        <div className="col-md-6">
+          <p className="info-text"><h1 style={{fontWeight: 'bolder', textAlign: 'center', color: '#056571', textTransform: 'uppercase'}}>Accountability</h1></p>
+          <hr/>
         </div>
-      </div>
-    </div>
-    <div className="matte-panel index-panel">
-      <div className="info-text container">
-        <h1>Share your goals</h1>
-        <p>If you want to share your goals to receieve encouragement along the way. </p>
-        <h1>Keep them private</h1>
-        <p> Accountabl will send you reminders and progress reports along the way.</p>
+        <div className="col-md-6">
+          <p className="info-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
     </div>
     <div className="fresh-panel index-panel">
-      <div className="container">
-        <div>Set Your Goals. </div>
-        <div>Achieve Them. </div>
+      <div className="row" style={{paddingTop : '75px'}}>
+        <div className="col-md-6">
+          <p className="info-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+        <div className="col-md-6">
+          <p className="info-text">
+            <h1 style={{fontWeight: 'bolder', textAlign: 'center', color: '#fff', textTransform: 'uppercase'}}>Consistency</h1>
+            <hr/>
+          </p>
+        </div>
       </div>
+    </div>
+    <div className="index-panel">
+        <h1 style={{textAlign: "center"}}>How It Works</h1>
+          <div className="row info-box">
+            <div className="col-md-3">
+              <div className="pic-div">
+                <img className="info-icon" src="https://image.flaticon.com/icons/svg/236/236803.svg"/>
+              </div>
+              <h3 className="info-box-title">Create a Goal</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="col-md-3">
+              <div className="pic-div">
+                <img className="info-icon" src="https://image.flaticon.com/icons/svg/236/236802.svg"/>
+              </div>
+              <h3 className="info-box-title">Configure Settings</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="col-md-3">
+              <div className="pic-div">
+                <img className="info-icon" src="https://image.flaticon.com/icons/svg/236/236850.svg"/>
+              </div>
+              <h3 className="info-box-title">Daily Reminders</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="col-md-3">
+              <div className="pic-div">
+                <img className="info-icon" src="https://image.flaticon.com/icons/svg/236/236800.svg"/>
+              </div>
+              <h3 className="info-box-title">Track Your Progress</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
     </div>
   </div>
 )
