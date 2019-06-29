8b109756c69757cd653797a45e6f934fff48233e
jae24 <jae.park510@gmail.com>
2019-06-28 22:13:21 -0700

    Started building the front-end of react client

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -4,18 +4,13 @@ import { Link } from 'react-router-dom'
 
 export default (props) => (
   <div>
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar main-nav">
       <h2 className="navbar-logo">Accountabl.</h2>
-      <Link to="/">
-        Home
-      </Link>
-      <Link to="/goals">
-        Goals
-      </Link>
-      <Link to="/about">
-        About
-      </Link>
-      <DeleteGoalsButton />
+      <div className="link-wrapper">
+        <Link to="/goals">Goals</Link>
+        <Link to="/about">About</Link>
+        <Link to="/">Home</Link>
+      </div>
     </nav>
   </div>
 )
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -2,8 +2,54 @@ import React from 'react';
 
 export default () => (
   <div>
-    <div>Hi</div>
-    <div>Hi</div>
-    <div>Hi</div>
+    <div className="hero matte-panel index-panel">
+      <div className="container">
+        <div className="hero-text">
+          <div>Set Your Goals. </div>
+          <div>Achieve Them. </div>
+        </div>
+      </div>
+    </div>
+    <div className="info1 fresh-panel index-panel">
+      <div className="container">
+        <div className="row info-box">
+          <div className="info-text col-md-4">
+            <div className="pic-div">
+              <img className="info-icon" src="https://image.flaticon.com/icons/png/512/236/236810.png"/>
+            </div>
+            <h3 className="info-box-title">Accountability</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+          <div className="info-text col-md-4">
+            <div className="pic-div">
+              <img className="info-icon" src="https://image.flaticon.com/icons/svg/236/236804.svg"/>
+            </div>
+            <h3 className="info-box-title">Consistency</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+          <div className="info-text col-md-4">
+            <div className="pic-div">
+              <img className="info-icon" src="https://image.flaticon.com/icons/svg/236/236804.svg"/>
+            </div>
+            <h3 className="info-box-title">Feedback</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="matte-panel index-panel">
+      <div className="info-text container">
+        <h1>Share your goals</h1>
+        <p>If you want to share your goals to receieve encouragement along the way. </p>
+        <h1>Keep them private</h1>
+        <p> Accountabl will send you reminders and progress reports along the way.</p>
+      </div>
+    </div>
+    <div className="fresh-panel index-panel">
+      <div className="container">
+        <div>Set Your Goals. </div>
+        <div>Achieve Them. </div>
+      </div>
+    </div>
   </div>
 )
